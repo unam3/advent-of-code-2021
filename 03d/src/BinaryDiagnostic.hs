59b894a0b0aa32fff8c8c02d8623d48fad2f1d58
Flowers Safety <40meta@gmail.com>
2021-12-03 17:01:55 +0600

    day 3 p2 solution

--- 03d/src/BinaryDiagnostic.hs
+++ 03d/src/BinaryDiagnostic.hs
@@ -1,6 +1,7 @@
 module BinaryDiagnostic where
 
 import Data.List (foldl', partition, transpose)
+import Debug.Trace (trace)
 
 parseInput :: String -> [String]
 parseInput = transpose . lines
@@ -61,13 +62,99 @@ solve = readFile "input.txt"
         . fmap getColumnGammaRate
         . parseInput
 
+
+determineMostCommonValue :: (Int, Int) -> Maybe Int
+determineMostCommonValue (zeroesCount, onesCount)
+    | zeroesCount == onesCount = Nothing
+    | zeroesCount > onesCount = Just 0
+    | otherwise = Just 1
+    
+
+o2BitCriteria :: String -> Char
+o2BitCriteria column =
+    -- "011110011100" -> ("00000","1111111")
+    let partitions = partition (== '0') column
+    in case determineMostCommonValue $ getTupleListsLenghts partitions of
+        Just 0 -> '0'
+        Just 1 -> '1'
+        -- If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+        Nothing -> '1'
+        nonMatched -> error $ "non matched" ++ show nonMatched
+
+filterFunction :: Int -> Char -> String -> Bool
+filterFunction position referenceBit string =
+    let bitInAPosition = (!!) string position
+    in bitInAPosition == referenceBit
+
+oxygenGeneratorRating' :: Int -> [String] -> [String]
+oxygenGeneratorRating' _ [onlyOneString] = [onlyOneString]
+oxygenGeneratorRating' position stringList =
+    let consideredColumnNumber = position
+        referenceBit = o2BitCriteria . (!! consideredColumnNumber) $ transpose stringList
+        result = filter (filterFunction position referenceBit) stringList
+    --in trace
+    --    (show (position, stringList, consideredColumnNumber, referenceBit))
+    --    $ oxygenGeneratorRating' (position + 1) result
+    in oxygenGeneratorRating' (position + 1) result
+        
+
+oxygenGeneratorRating :: String -> Int
+oxygenGeneratorRating =
+    getDecimal
+        . fmap (
+            (read :: String -> Int)
+                . (:[])
+            )
+        . head
+        . oxygenGeneratorRating' 0
+        . lines
+
+cO2ScrubberCriteria :: String -> Char
+cO2ScrubberCriteria column =
+    let partitions = partition (== '0') column
+    in case determineMostCommonValue $ getTupleListsLenghts partitions of
+        Just 0 -> '1'
+        Just 1 -> '0'
+        -- If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+        Nothing -> '0'
+        nonMatched -> error $ "non matched" ++ show nonMatched
+
+cO2ScrubberRating' :: Int -> [String] -> [String]
+cO2ScrubberRating' _ [onlyOneString] = [onlyOneString]
+cO2ScrubberRating' position stringList =
+    let consideredColumnNumber = position
+        referenceBit = cO2ScrubberCriteria . (!! consideredColumnNumber) $ transpose stringList
+        result = filter (filterFunction position referenceBit) stringList
+    --in trace
+    --    (show (position, stringList, consideredColumnNumber, referenceBit))
+    --    $ cO2ScrubberRating' (position + 1) result
+    in cO2ScrubberRating' (position + 1) result
+        
+
+cO2ScrubberRating :: String -> Int
+cO2ScrubberRating =
+    getDecimal
+        . fmap (
+            (read :: String -> Int)
+                . (:[])
+            )
+        . head
+        . cO2ScrubberRating' 0
+        . lines
+
+getLifeSupportRating :: String -> Int
+-- can we use point-free here?
+--getLifeSupportRating = fmap (\f -> f) [oxygenGeneratorRating, cO2ScrubberRating]
+getLifeSupportRating inputString =
+    oxygenGeneratorRating inputString * cO2ScrubberRating inputString
+
 solveTest2 :: IO ()
 solveTest2 = readFile "testInput"
     >>= print
-        . parseInput
+        . getLifeSupportRating
 
 solve2 :: IO ()
 solve2 = readFile "input.txt"
     >>= print
-        . parseInput
+        . getLifeSupportRating
 
