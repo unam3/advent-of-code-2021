c2743675e2f55ec3ab16807aec3c3aa20c600449
Flowers Safety <40meta@gmail.com>
2022-01-05 14:14:17 +0600

    day 6 p2 solution issue was fixed

--- 06d/src/Lanternfish.hs
+++ 06d/src/Lanternfish.hs
@@ -1,62 +1,100 @@
 module Lanternfish where
 
 import Data.List ((!!), foldl')
+import Debug.Trace (trace)
+import Data.Map.Strict (Map, empty, foldlWithKey', insert, insertWith, lookup, member)
 import GHC.List (iterate')
-import Prelude hiding ((!!))
-
-parseInput :: String -> [Int]
-parseInput = read . ("[" ++) . (++ "]")
-
-type CreateNewLanternfish = Bool
-
-simulateDayCycle' :: Int -> (Int, CreateNewLanternfish)
-simulateDayCycle' 0 = (6, True)
-simulateDayCycle' number = (number - 1, False) 
-
--- theNumber — number that represents the number of days until it creates a new lanternfish
-foldlF :: ([Int], Int) -> Int -> ([Int], Int)
-foldlF (accList, numberOfFishToAdd) theNumber =
-    let (nextTheNumber, doCreateNewLanternfish) = simulateDayCycle' theNumber
-    in (
-        nextTheNumber : accList,
-        (if doCreateNewLanternfish
-            then numberOfFishToAdd + 1
-            else numberOfFishToAdd
-        )
-    )
-
-simulateDayCycle :: [Int] -> [Int]
-simulateDayCycle theNumbersList =
-    let (newTheNumbersList, numberOfFishToAdd) = foldl' foldlF ([], 0) theNumbersList
-    in (reverse newTheNumbersList) ++ (replicate numberOfFishToAdd 8)
-
-simulateNDayCycles :: Int -> [Int] -> [Int]
-simulateNDayCycles dayCyclesNumber theNumbersList = (!!) (iterate' simulateDayCycle theNumbersList) dayCyclesNumber
-
-getNumberOfLanternsifhAfterNDays :: Int -> String -> Int
-getNumberOfLanternsifhAfterNDays dayCyclesNumber theNumbersString =
-    length  
-        . simulateNDayCycles dayCyclesNumber
-        $ parseInput theNumbersString
+import Prelude hiding ((!!), lookup)
 
 
-solveTest :: IO ()
-solveTest = readFile "testInput"
-    >>= print
-        . getNumberOfLanternsifhAfterNDays 80
+type InternalTimer = Int
+type TimersCount = Int
+type LanternFishMap = Map InternalTimer TimersCount
 
-solve :: IO ()
-solve = readFile "input.txt"
-    >>= print
-        . getNumberOfLanternsifhAfterNDays 80
+parseString :: String -> [Int]
+parseString = read . ("[" ++) . (++ "]")
 
-solveTest2 :: IO ()
-solveTest2 = readFile "testInput"
-    >>= print
-        . parseInput
 
-solve2 :: IO ()
-solve2 = readFile "input.txt"
-    >>= print
-        . parseInput
+foldlF :: LanternFishMap -> InternalTimer -> LanternFishMap
+foldlF lanternFishMap internalTimer = insertWith (+) internalTimer 1 lanternFishMap
 
+makeLanternFishMap :: [Int] -> LanternFishMap
+makeLanternFishMap = foldl' foldlF empty
+
+parseInput :: String -> LanternFishMap
+parseInput = makeLanternFishMap . parseString
+
+
+{-
+0 —— 13  ——> 0 —— 5
+1 —— 5   ——> 1 —— ~ 
+..       ——> ..    
+5 —— 11  ——> 5 —— 32
+6 —— 32  ——> 6 —— 29 + 13 (0)
+7 —— 29  ——> 7 —— 17
+8 —— 17  ——> 8 —— 13
+-}
+foldlWithKeyF :: (LanternFishMap, LanternFishMap) -> InternalTimer -> TimersCount -> (LanternFishMap, LanternFishMap)
+foldlWithKeyF (oldLanternFishMap, newLanternFishMap) internalTimer timersCount =
+    -- if we have 0 -> lookup 7: 7 + 0 or 0
+    -- noop on 0
+    let newMap =
+            (if internalTimer == 0
+            -- timersCount of 0 goes to 6 + timerscount of 7
+            -- goes timersCount to 8
+            then case lookup 7 oldLanternFishMap of
+                Nothing ->
+                    trace
+                        (show ("nothing", timersCount, oldLanternFishMap, newLanternFishMap))
+                        $ insert 8 timersCount
+                            $ insert 6 timersCount newLanternFishMap
+                Just sevenTimersCount ->
+                    trace
+                        (show (sevenTimersCount, '+', timersCount, oldLanternFishMap, newLanternFishMap))
+                        $ insert 8 timersCount
+                            $ insert 6 (sevenTimersCount + timersCount) newLanternFishMap
+            else if internalTimer == 7
+                then if member 0 oldLanternFishMap
+                    then trace
+                        "internalTimer == 7 — noop"
+                        newLanternFishMap -- noop
+                    else insert 6 timersCount newLanternFishMap
+                -- timersCount goes to internalTimer - 1
+                else trace
+                        (show (internalTimer - 1, timersCount, oldLanternFishMap, newLanternFishMap))
+                        $ insert (internalTimer - 1) timersCount newLanternFishMap)
+    in (oldLanternFishMap, newMap)
+
+simulateDayCycle :: LanternFishMap -> LanternFishMap
+simulateDayCycle lanternFishMap = snd $ foldlWithKey' foldlWithKeyF (lanternFishMap, empty) lanternFishMap
+
+simulateNDayCycles :: Int -> LanternFishMap -> LanternFishMap
+simulateNDayCycles dayCyclesNumber lanternFishMap = (!!) (iterate' simulateDayCycle lanternFishMap) dayCyclesNumber
+
+--getNumberOfLanternsifhAfterNDays :: Int -> String -> Int
+--getNumberOfLanternsifhAfterNDays dayCyclesNumber theNumbersString =
+--    length  
+--        . simulateNDayCycles dayCyclesNumber
+--        $ parseInput theNumbersString
+--
+--
+--solveTest :: IO ()
+--solveTest = readFile "testInput"
+--    >>= print
+--        . getNumberOfLanternsifhAfterNDays 80
+--
+--solve :: IO ()
+--solve = readFile "input.txt"
+--    >>= print
+--        . getNumberOfLanternsifhAfterNDays 80
+--
+--solveTest2 :: IO ()
+--solveTest2 = readFile "testInput"
+--    >>= print
+--        . getNumberOfLanternsifhAfterNDays 256
+--
+--solve2 :: IO ()
+--solve2 = readFile "input.txt"
+--    >>= print
+--        . parseInput
+--
--- 06d/test/LanternfishSpec.hs
+++ 06d/test/LanternfishSpec.hs
@@ -1,8 +1,14 @@
 module LanternfishSpec where 
 
+import Data.Char (digitToInt)
+import Data.List (group, sort)
+import Data.Map.Strict (fromList)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 import Lanternfish
+
+parsePuzzleTestOutputString :: String -> [(Int, Int)]
+parsePuzzleTestOutputString = fmap (\ n -> (digitToInt $ head n, length n)) . tail . group . sort
 
 spec :: Spec
 spec = do
@@ -10,10 +16,73 @@ spec = do
         it "works"
             $ shouldBe
                 (parseInput "3,4,3,1,2")
-                [3,4,3,1,2]
+                (fromList [(3,2),(4,1),(1,1),(2,1)])
 
-    describe "simulateNDayCycles" $ do
-        it "works"
+    describe "simulateDayCycle" $ do
+        --it "works"
+        --    $ shouldBe
+        --        (simulateDayCycle $ parseInput "3,4,3,1,2")
+        --        (fromList [(2,2),(3,1),(1,1),(0,1)])
+
+        it "simulateNDayCycles 6: narrowed down"
+            $ shouldBe
+                (simulateDayCycle $ fromList [(6,2),(7,2),(8,1)])
+                --(simulateNDayCycles 1 $ fromList [(6,2),(7,2),(8,1)])
+                (fromList $ parsePuzzleTestOutputString "5,5,6,6,7")
+          
+    describe "simulateNDayCycle" $ do
+        --it "simulateNDayCycles 2"
+        --    $ shouldBe
+        --        (simulateNDayCycles 2 (parseInput "3,4,3,1,2"))
+        --        (fromList $ parsePuzzleTestOutputString "1,2,1,6,0,8")
+          
+        --it "simulateNDayCycles 3"
+        --    $ shouldBe
+        --        (simulateNDayCycles 3 (parseInput "3,4,3,1,2"))
+        --        (fromList $ parsePuzzleTestOutputString "0,1,0,5,6,7,8")
+
+        --it "simulateNDayCycles 4"
+        --    $ shouldBe
+        --        (simulateNDayCycles 4 (parseInput "3,4,3,1,2"))
+        --        (fromList $ parsePuzzleTestOutputString "6,0,6,4,5,6,7,8,8")
+
+        --it "simulateNDayCycles 4 #2"
+        --    $ shouldBe
+        --        (simulateNDayCycles 1 (parseInput "0,1,0,5,6,7,8"))
+        --        (fromList $ parsePuzzleTestOutputString "6,0,6,4,5,6,7,8,8")
+          
+        --it "simulateNDayCycles 4: narrowed down issue"
+        --    $ shouldBe
+        --        (simulateNDayCycles 1 (parseInput "0,0,6,7"))
+        --        (fromList $ parsePuzzleTestOutputString "6,6,5,6,8,8")
+
+        --it "simulateNDayCycles 5"
+        --    $ shouldBe
+        --        (simulateNDayCycles 5 (parseInput "3,4,3,1,2"))
+        --        -- [(3,1),(4,1),(5,3),(6,2),(7,2),(8,1)]
+        --        (fromList $ parsePuzzleTestOutputString "5,6,5,3,4,5,6,7,7,8")
+
+    --  it "simulateNDayCycles 6: narrowed down"
+    --        $ shouldBe
+    --            (simulateNDayCycles 1 $ fromList [(6,2),(7,2),(8,1)])
+    --            (fromList $ parsePuzzleTestOutputString "5,5,6,6,7")
+
+        --it "simulateNDayCycles 6"
+        --    $ shouldBe
+        --        (simulateNDayCycles 6 (parseInput "3,4,3,1,2"))
+        --        (fromList $ parsePuzzleTestOutputString "4,5,4,2,3,4,5,6,6,7")
+
+        --it "simulateNDayCycles 7"
+        --    $ shouldBe
+        --        (simulateNDayCycles 7 (parseInput "3,4,3,1,2"))
+        --        (fromList $ parsePuzzleTestOutputString "3,4,3,1,2,3,4,5,5,6")
+
+        --it "simulateNDayCycles 9"
+        --    $ shouldBe
+        --        (simulateNDayCycles 9 (parseInput "3,4,3,1,2"))
+        --        (fromList $ parsePuzzleTestOutputString "1,2,1,6,0,1,2,3,3,4,8")
+
+        it "simulateNDayCycles 18"
             $ shouldBe
                 (simulateNDayCycles 18 (parseInput "3,4,3,1,2"))
-                [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8]
+                (fromList [(6, 5),(0,3),(4,2),(5,1),(1,5),(2,3),(3,2),(7,1),(8,4)])
