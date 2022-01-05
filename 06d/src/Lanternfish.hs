0c007f3d22fcfc84a47074fecc6276d49c62d216
Flowers Safety <40meta@gmail.com>
2022-01-05 14:20:04 +0600

    day 6 p2 solution was implemented

--- 06d/src/Lanternfish.hs
+++ 06d/src/Lanternfish.hs
@@ -1,7 +1,6 @@
 module Lanternfish where
 
 import Data.List ((!!), foldl')
-import Debug.Trace (trace)
 import Data.Map.Strict (Map, empty, foldlWithKey', insert, insertWith, lookup, member)
 import GHC.List (iterate')
 import Prelude hiding ((!!), lookup)
@@ -36,33 +35,21 @@ parseInput = makeLanternFishMap . parseString
 -}
 foldlWithKeyF :: (LanternFishMap, LanternFishMap) -> InternalTimer -> TimersCount -> (LanternFishMap, LanternFishMap)
 foldlWithKeyF (oldLanternFishMap, newLanternFishMap) internalTimer timersCount =
-    -- if we have 0 -> lookup 7: 7 + 0 or 0
-    -- noop on 0
-    let newMap =
-            (if internalTimer == 0
-            -- timersCount of 0 goes to 6 + timerscount of 7
-            -- goes timersCount to 8
-            then case lookup 7 oldLanternFishMap of
+    let newMap |
+            internalTimer == 0
+            = case lookup 7 oldLanternFishMap of
                 Nothing ->
-                    trace
-                        (show ("nothing", timersCount, oldLanternFishMap, newLanternFishMap))
-                        $ insert 8 timersCount
-                            $ insert 6 timersCount newLanternFishMap
+                    insert 8 timersCount
+                        $ insert 6 timersCount newLanternFishMap
                 Just sevenTimersCount ->
-                    trace
-                        (show (sevenTimersCount, '+', timersCount, oldLanternFishMap, newLanternFishMap))
-                        $ insert 8 timersCount
-                            $ insert 6 (sevenTimersCount + timersCount) newLanternFishMap
-            else if internalTimer == 7
-                then if member 0 oldLanternFishMap
-                    then trace
-                        "internalTimer == 7 — noop"
-                        newLanternFishMap -- noop
+                    insert 8 timersCount
+                        $ insert 6 (sevenTimersCount + timersCount) newLanternFishMap
+            | internalTimer == 7
+            = if member 0 oldLanternFishMap
+                    then newLanternFishMap
                     else insert 6 timersCount newLanternFishMap
-                -- timersCount goes to internalTimer - 1
-                else trace
-                        (show (internalTimer - 1, timersCount, oldLanternFishMap, newLanternFishMap))
-                        $ insert (internalTimer - 1) timersCount newLanternFishMap)
+            | otherwise
+            = insert (internalTimer - 1) timersCount newLanternFishMap
     in (oldLanternFishMap, newMap)
 
 simulateDayCycle :: LanternFishMap -> LanternFishMap
@@ -71,30 +58,29 @@ simulateDayCycle lanternFishMap = snd $ foldlWithKey' foldlWithKeyF (lanternFish
 simulateNDayCycles :: Int -> LanternFishMap -> LanternFishMap
 simulateNDayCycles dayCyclesNumber lanternFishMap = (!!) (iterate' simulateDayCycle lanternFishMap) dayCyclesNumber
 
---getNumberOfLanternsifhAfterNDays :: Int -> String -> Int
---getNumberOfLanternsifhAfterNDays dayCyclesNumber theNumbersString =
---    length  
---        . simulateNDayCycles dayCyclesNumber
---        $ parseInput theNumbersString
---
---
---solveTest :: IO ()
---solveTest = readFile "testInput"
---    >>= print
---        . getNumberOfLanternsifhAfterNDays 80
---
---solve :: IO ()
---solve = readFile "input.txt"
---    >>= print
---        . getNumberOfLanternsifhAfterNDays 80
---
---solveTest2 :: IO ()
---solveTest2 = readFile "testInput"
---    >>= print
---        . getNumberOfLanternsifhAfterNDays 256
---
---solve2 :: IO ()
---solve2 = readFile "input.txt"
---    >>= print
---        . parseInput
---
+getNumberOfLanternsifhAfterNDays :: Int -> String -> Int
+getNumberOfLanternsifhAfterNDays dayCyclesNumber theNumbersString =
+    foldl' (+) 0
+        . simulateNDayCycles dayCyclesNumber
+        $ parseInput theNumbersString
+
+
+solveTest :: IO ()
+solveTest = readFile "testInput"
+    >>= print
+        . getNumberOfLanternsifhAfterNDays 80
+
+solve :: IO ()
+solve = readFile "input.txt"
+    >>= print
+        . getNumberOfLanternsifhAfterNDays 80
+
+solveTest2 :: IO ()
+solveTest2 = readFile "testInput"
+    >>= print
+        . getNumberOfLanternsifhAfterNDays 256
+
+solve2 :: IO ()
+solve2 = readFile "input.txt"
+    >>= print
+        . getNumberOfLanternsifhAfterNDays 256
