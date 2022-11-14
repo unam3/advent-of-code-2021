ab1663ef03f59a9bb6424ef3c998d0713c188603
Flowers Safety <40meta@gmail.com>
2022-11-14 17:53:12 +0600

    constructPath works for testInput2
    To automate tests results comparison one can (1) strip output of constructPath, (2) sort it and (3) run diff on it and (4) sorted reference output

--- 12d/test/PassagePathingSpec.hs
+++ 12d/test/PassagePathingSpec.hs
@@ -14,7 +14,7 @@ spec :: Spec
 spec = do
     testInput <- runIO $ readFile "testInput"
 
-    --testInput2 <- runIO $ readFile "testInput2"
+    testInput2 <- runIO $ readFile "testInput2"
 
     --testInput3 <- runIO $ readFile "testInput3"
 
@@ -45,3 +45,7 @@ spec = do
             $ shouldBe
                 (constructPathsWrapper $ parseInput testInput)
                 [["start","A","c","A","b","A","end"],["start","A","c","A","b","end"],["start","A","c","A","end"],["start","A","b","A","c","A","end"],["start","A","b","A","end"],["start","A","b","end"],["start","A","end"],["start","b","A","c","A","end"],["start","b","A","end"],["start","b","end"]]
+        it "construct paths for testInput2"
+            $ shouldBe
+                (constructPathsWrapper $ parseInput testInput2)
+                [["start","HN","dc","end"],["start","HN","dc","HN","end"],["start","HN","dc","HN","kj","HN","end"],["start","HN","dc","kj","HN","end"],["start","HN","end"],["start","HN","kj","HN","dc","end"],["start","HN","kj","HN","dc","HN","end"],["start","HN","kj","HN","end"],["start","HN","kj","dc","end"],["start","HN","kj","dc","HN","end"],["start","kj","HN","dc","end"],["start","kj","HN","dc","HN","end"],["start","kj","HN","end"],["start","kj","dc","end"],["start","kj","dc","HN","end"],["start","dc","end"],["start","dc","HN","end"],["start","dc","HN","kj","HN","end"],["start","dc","kj","HN","end"]]
